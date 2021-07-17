c606ffe3960335ab480cddba36a26620324972e4
pjhq2 <pkonu7@gmail.com>
2021-07-17 19:05:44 +0900

    DFS, BFS

---
+++ DFS.py
@@ -0,0 +1,29 @@
+### DFS 예제(p.142)
+# DFS는 stack을 이용하는 알고리즘 - 재귀 함수를 이용해서 매우 간결하게 구현할 수 있다.
+# DFS 메서드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
